5f806dfd4fd80961333af7b685844423cd271a93
kalikokalikova <calico.seders@gmail.com>
2023-08-01 15:13:29 -0700

    change name to landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Wall from "./pages/Wall";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Welcome from "./pages/Welcome";
+import Landing from "./pages/Landing";
 import Account from "./pages/Account";
 import FlyerFormPage from "./pages/FlyerFormPage";
 import FlyerPage from "./pages/FlyerPage";
@@ -14,7 +14,7 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Welcome />} />
+            <Route path="/" element={<Landing />} />
             <Route path="/wall" element={<Wall />} />
             <Route path="/account" element={<Account />} />
             <Route path="/flyers/:id" element={<FlyerPage />} />
